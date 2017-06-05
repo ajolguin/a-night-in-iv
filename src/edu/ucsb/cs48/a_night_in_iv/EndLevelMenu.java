f32a408e47ee79881a3189ec3937611b7bdedc14
Karl Wang <kovl.void@gmail.com>
2017-06-04 21:14:09 -0700

    fix quit game when losing

--- src/edu/ucsb/cs48/a_night_in_iv/EndLevelMenu.java
+++ src/edu/ucsb/cs48/a_night_in_iv/EndLevelMenu.java
@@ -1,6 +1,9 @@
 package edu.ucsb.cs48.a_night_in_iv;
 
 import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Matthew R on 5/28/2017.
@@ -13,25 +16,29 @@ public class EndLevelMenu extends JOptionPane {
     public final int NEXT_LEVEL = 2;
     public final int QUIT_GAME = 3;
 
+    public final List<String> ol = Arrays.asList("Return to Main Menu", "Restart level", "Next level", "Quit game");
+
     public EndLevelMenu(){
         super();
     }
 
     public int showWinDialog(JFrame parent){
-        String[] options = {"Return to Main Menu", "Restart level", "Next level", "Quit game"};
-        return showOptionDialog(parent,
+        String[] options = {ol.get(0), ol.get(1), ol.get(2), ol.get(3)};
+        int selected_option = showOptionDialog(parent,
                 "You've survived the perils of IV.                                   SWAG",
                 "", JOptionPane.DEFAULT_OPTION,
                 JOptionPane.INFORMATION_MESSAGE, null,
                 options, options[0]);
+        return ol.indexOf(options[selected_option]);
     }
 
     public int showLoseDialog(JFrame parent){
-        String[] options = {"Return to Main Menu", "Restart level", "Quit game"};
-        return showOptionDialog(parent,
+        String[] options = {ol.get(0), ol.get(1), ol.get(3)};
+        int selected_option = showOptionDialog(parent,
                 "YOU BOOZE, YOU LOSE!!!",
                 "", JOptionPane.DEFAULT_OPTION,
                 JOptionPane.INFORMATION_MESSAGE, null,
                 options, options[0]);
+        return ol.indexOf(options[selected_option]);
     }
 }
